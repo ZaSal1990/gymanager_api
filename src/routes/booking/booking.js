6707bf1eabf11b331912a8ee67bdb45326589332
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-10-03 13:00:34 -0600

    add booking post get route, fix capacity routes order

--- src/routes/booking/booking.js
+++ src/routes/booking/booking.js
@@ -16,7 +16,7 @@ module.exports = (db) => {
     JOIN users ON room_bookings.user_id = users.id
     JOIN days ON room_bookings.day_id = days.id;`)
       .then(data =>
-        res.json({admin: data.rows[0]})
+        res.json({booking: data.rows})
       )
       .catch(err => {
         res
@@ -24,10 +24,11 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
   router.post("/", (req, res) => {
-    db.query(``)
+    db.query(`INSERT INTO room_bookings (room_id, user_id, day_id, time) VALUES ($1, $2, $3, $4) RETURNING *;`, [1, 1, 1, '7pm']) //hard code params
       .then(data =>
-        res.json({admin: data.rows[0]})
+        res.json({roomBookingId: data.rows})
       )
       .catch(err => {
         res
@@ -35,6 +36,7 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
   return router;
 };
 
